a3227d909f5bdc533b144e4b28b6e3bca874028c
Emanoel <emanoel.pluto@gmail.com>
2026-07-24 11:02:09 -0300

    building String Methods.py

---
+++ StringMethods.py
@@ -0,0 +1,10 @@
+nome = "Emanoel HENRique    da faina         "
+sobrenome = "EMANOEL HENRIQUE DA FAINA"
+print(sobrenome)
+
+print("Em lower case: \n" +sobrenome.lower())
+
+print(nome)
+print("Em upper case: \n" +nome.upper())
+print("\nSem os espaços: \n" +nome.strip())
+print("\nPondo vírgula: \n" + nome.replace("da", ",da").strip())
