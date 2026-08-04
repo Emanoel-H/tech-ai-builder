ea8e9055dce738c783cac8b558a675aaa2b1246f
Emanoel <emanoel.pluto@gmail.com>
2026-08-04 08:10:13 -0300

    adding while loop to ChatBot interaction

--- ChatBot.py
+++ ChatBot.py
@@ -7,7 +7,10 @@ os.environ["GEMINI_API_KEY"] = str(os.getenv("GEMINI_API_KEY"))
 client = genai.Client()
 
 chat = client.chats.create(model="gemini-3.5-flash")
-
-response = chat.send_message("Com poucas palavras, escreva para Rhaissa Melo o quanto eu sinto por ela")
-
-print(response.text)
+continuar = True
+while(continuar):
+    prompt = input("Faça uma pergunta ao chat: ")
+    response = chat.send_message(prompt)
+    print(response.text)
+    answer = input("Quer continuar conversando com o chat? \n Digite \n 1 - Sim \n 0 - Não  ")
+    continuar = answer == "1"
