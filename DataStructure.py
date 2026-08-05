0c1e09d388ce9301768e044639b7afc01453e058
Emanoel <emanoel.pluto@gmail.com>
2026-08-05 09:14:07 -0300

    refactoring loop to remove small means

--- DataStructure.py
+++ DataStructure.py
@@ -10,8 +10,12 @@ i = 0
 
 while i < len(means_list):
     means_list[i] = means_list[i] + 1
-    if means_list[i] > 10:
-        means_list[i] = 10
+    
+    means_list[i] = means_list[i] = 10 if means_list[i] > 10 else means_list[i]
+
+    if means_list[i] < 5:
+        means_list.remove(means_list[i])
+
     i = i + 1
 
 for i in means_list:
