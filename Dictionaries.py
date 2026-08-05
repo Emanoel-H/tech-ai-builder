4a5ca44dd18d07f9f4fe9764ee4540f7a980a5be
Emanoel <emanoel.pluto@gmail.com>
2026-08-05 10:10:48 -0300

    giving students their means

---
+++ Dictionaries.py
@@ -0,0 +1,22 @@
+students = {}
+
+stop = "yes"
+
+while stop == "yes":
+    name = input("Enter your name: ")
+    mean = float(input("Enter a mean number: "))
+    students[name] = mean
+    stop = input("Would you like to continue (yes/no): ")
+
+
+for name in list(students.keys()):
+    new_mean = students[name] + 1
+
+    students[name] = students[name] = 10 if new_mean > 10 else new_mean
+
+    if students[name] < 5:
+        print(f"{name} is reproved for small mean")
+        students.__delitem__(name)
+
+
+print(students)
