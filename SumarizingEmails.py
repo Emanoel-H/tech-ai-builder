a9409cab9a47205efb4f5f6578fa0d4f4e13431a
Emanoel <emanoel.pluto@gmail.com>
2026-08-06 06:55:04 -0300

    writing email summaries in a file

--- SumarizingEmails.py
+++ SumarizingEmails.py
@@ -1,7 +1,6 @@
 import os
 
 from google import genai
-from rich import prompt
 
 emails = [
     {
@@ -54,12 +53,14 @@ def summarize_emails(list_of_emails):
         summary_list.append(f"{i + 1}º Email: {response.text}")
         print("-" * 50)
 
-        return summary_list
+    return summary_list
 
     # mails.append(f"Sender: {sender} \nMessage: {body}\n")
     # prompt = "I want you to summarize in a few words what the following e-mails want to say: \n ".join(mails)
 
+summaries = summarize_emails(emails)
 
-
-summarize_emails(emails)
+with open("C:\Users\henri\Desktop\emails.txt", "w", encoding="utf-8") as file:
+    for email in summaries:
+        file.write(f"{email}\n")
 
