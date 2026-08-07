e60596db2881e3c175e9f07373731e0abab73500
Emanoel <emanoel.pluto@gmail.com>
2026-08-07 16:52:54 -0300

    handling TypeError exception

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,4 +1,12 @@
 try:
     age = int(input("How old are you? "))
+
+    people = 3
+
+    people_2 = "John, Mary and Joseph"
+
+    people + people_2
 except ValueError:
     print("Oops! That isn't an integer!")
+except TypeError:
+    print("Oops! you cannot sum a number with a string!")
