b169d67d327a1726e1f24bdca2d6483ce1db7892
Emanoel <emanoel.pluto@gmail.com>
2026-08-07 16:54:44 -0300

    printing error details

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,12 +1,13 @@
 try:
     age = int(input("How old are you? "))
 
-    people = 3
-
-    people_2 = "John, Mary and Joseph"
-
-    people + people_2
-except ValueError:
+    # people = 3
+    #
+    # people_2 = "John, Mary and Joseph"
+    #
+    # people + people_2
+except ValueError as ve:
     print("Oops! That isn't an integer!")
+    print("Error details:", ve)
 except TypeError:
     print("Oops! you cannot sum a number with a string!")
